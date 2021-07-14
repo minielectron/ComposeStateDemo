5b52717713ea989140cb0f9b2b50498cf835303a
Prakash Sharma <prakash009Kp@gmail.com>
2021-07-14 16:26:01 +0530

    Add button and handle click listener

--- app/src/main/java/com/apd/composestate/MainActivity.kt
+++ app/src/main/java/com/apd/composestate/MainActivity.kt
@@ -3,11 +3,16 @@ package com.apd.composestate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.apd.composestate.ui.theme.ComposeStateTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +20,66 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStateTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MainScreen() {
+    Surface(
+        color = Color.LightGray,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            var text: String by remember {
+                mutableStateOf("")
+            }
+            var showOutput by remember {
+                mutableStateOf(false)
+            }
+
+            NameInput(text, onNameChange = { text = it })
+            Spacer(modifier = Modifier.height(10.dp))
+            if (showOutput) {
+                NameOutput(name = text)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(onClick = {
+                showOutput = true
+            }) {
+                Text(text = "Show output")
+            }
+        }
+
+    }
+}
+
+@Composable
+fun NameInput(name: String, onNameChange: (value: String) -> Unit) {
+    TextField(
+        value = name,
+        onValueChange = onNameChange,
+        label = { Text(text = "Enter name") },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun NameOutput(name: String) {
+    Text(text = name)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeStateTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
